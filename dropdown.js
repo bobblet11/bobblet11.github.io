015de802e7b7c795b3540b10d2b8dd83404e6036
bobblet11 <benglover78@gmail.com>
2025-03-06 23:31:14 +0800

    feat(dropdown): mobile version (blue) done

--- dropdown.js
+++ dropdown.js
@@ -1,5 +1,17 @@
 const dropdown = document.getElementById("dropdown");
-
+const dropdownContent = document.getElementById("dropdown-content");
 dropdown.addEventListener("click", () => {
         dropdown.classList.toggle("active");
+        dropdownContent.classList.toggle("active");
 });
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+        if (
+                dropdown.classList.contains("active") &&
+                !dropdown.contains(event.target)
+        ) {
+                dropdownContent.classList.remove("active");
+                dropdown.classList.remove("active");
+        }
+};
